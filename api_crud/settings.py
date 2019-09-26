7efbbb7b20d2d6ece8a8d6022c0ad626882f02d6
Samuel P <samuel.p991@gmail.com>
2019-09-27 00:27:27 +0200

    fix statics

--- api_crud/settings.py
+++ api_crud/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 LOGGING = {
     'version': 1,
