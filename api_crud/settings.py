42eaf55ed40533b93e4118a23ec67c5a43a49898
Samuel P <samuel.p991@gmail.com>
2019-10-07 01:32:22 +0200

    add logging

--- api_crud/settings.py
+++ api_crud/settings.py
@@ -168,21 +168,21 @@ STATICFILES_FINDERS = (
 )
 AWS_DEFAULT_ACL = None
 
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'console': {
-#             'class': 'logging.StreamHandler',
-#         },
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['console'],
-#              'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-#         },
-#     },
-# }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
 
 try:
     from .local_settings import *
